d3a50edb0f37d6172dd40d14098ffc745dc607ce
Colin Rubbert <colinrubbert@gmail.com>
2015-09-23 23:00:22 -0500

    Build functioning signature process

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -9,6 +9,15 @@ class SignaturesController < ApplicationController
   end
 
   def create
+    embedded_request = create_embedded_request(name: params[:name], email_address: params[:email_address])
+    @sign_url = get_sign_url(embedded_request)
+    render :embedded_signature
+  end
+
+
+  private
+
+  def create_embedded_request(options = {})
     client = HelloSign::Client.new :api_key => ENV["hellosign_api_key"]
     client.create_embedded_signature_request_with_template(
     :test_mode => 1,             #Set to 1 to signify this is just a test
@@ -18,16 +27,22 @@ class SignaturesController < ApplicationController
     :message => "Please fill out all relevant information, sign, date.",
     :signers => [
         {
-          :name => "Colin Rubbert",
-          :email_address => "colinrubbert@gmail.com",
+          :name => options[:name],
+          :email_address => options[:email_address],
           :role => 'Agent'
         }
       ],
     )
+  end
 
-    embedded = client.get_embedded_sign_url :signature_id => '72f17b7a021381aebc7681a638371bbe'
-    @sign_url = embedded.sign_url
-    render :embedded_signature
+  def get_sign_url(embedded_request)
+    sign_id = get_signature_id(embedded_request)
+    HelloSign.get_embedded_sign_url(signature_id: sign_id).sign_url
+  end
+
+  def get_signature_id(embedded_request)
+    # byebug
+    embedded_request.signatures[0].signature_id
   end
 
 end
